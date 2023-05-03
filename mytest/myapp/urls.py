02c1af0defff8671ab6a58b9b9d4f2ee04ed88c8
another-worlds <a.egorshev@gmail.com>
2023-05-03 20:46:05 +0600

    Milestone 4.a.ii completed

--- mytest/myapp/urls.py
+++ mytest/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index_url')
+    path('', views.index, name='index_url'),
+    path('<int:post_id>', views.details, name='details_url')
 ]
--- mytest/myapp/views.py
+++ mytest/myapp/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from .models import Post, Comment
 
-# Create your views here.
+# Temporary helper function for handling untemplated responses
+def view_item(score, title, tag="h3"):
+    return f"<{tag}>{score} | {title}</{tag}>"
+
+
+# View 5 latest posts 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:5]
     result = []
     for i in range(len(posts)):
-        result.append(f"<h3>{posts[i].score} | {posts[i].title}<h3>")
+        result.append(view_item(posts[i].score, posts[i].title))
+    "<br>".join(result)
+    return HttpResponse(result)
+
+# View selected post and it's comments
+def details(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+        comments = post.comment_set.all()
+        result = []
+        result.append(view_item(post.score, post.title))
+        if comments:
+            for comment in comments:
+                result.append(view_item(comment.score, comment.author, tag='h4'))
+        else:
+            result.append("Post wasn't commented yet")
+    except Post.DoesNotExist:
+        raise Http404
     "<br>".join(result)
     return HttpResponse(result)
