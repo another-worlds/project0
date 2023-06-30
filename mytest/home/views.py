8647b36752242aaa948cd08c04d426155739b27b
another-worlds <a.egorshev@gmail.com>
2023-06-30 19:40:25 +0600

    minor fix for assignment 13.

--- mytest/home/views.py
+++ mytest/home/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def myview(request):
-    resposne = HttpResponse('d26e5e36')
+    response = HttpResponse('d26e5e36')
     response.set_cookie('dj4e_cookie', 'd26e5e36', max_age=1000)
+    return response
     
