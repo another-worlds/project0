5406851a41b6ebb3f35b70b40fcdb72e4f8720ad
another-worlds <a.egorshev@gmail.com>
2023-06-30 22:17:40 +0600

    final

--- mytest/hello/views.py
+++ mytest/hello/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def myview(request):
-    response = HttpResponse('d26e5e36')
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+    response = HttpResponse(f'view count={num_visits}    d26e5e36')
+    if num_visits >= 3: del(request.session['num_visits'])
     response.set_cookie('dj4e_cookie', 'd26e5e36', max_age=1000)
     return response
