1df1df17fbbbffe83bbf933b693bc819f1895f4a
another-worlds <a.egorshev@gmail.com>
2023-07-10 09:18:59 +0600

    Milestone 5.a.i and 5.a.ii completed

--- mytest/hello/urls.py
+++ mytest/hello/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'hello'
 urlpatterns = [
-    path('', views.myview),
+    path('', views.myview, name='index_url'),
+    path('test', views.a_view, name='test_utl')
 ]
--- mytest/hello/views.py
+++ mytest/hello/views.py
@@ -10,3 +10,21 @@ def myview(request):
     if num_visits >= 3: del(request.session['num_visits'])
     response.set_cookie('dj4e_cookie', 'd26e5e36', max_age=1000)
     return response
+
+def a_view(request):
+    # increment or cretae "num_requests" variable
+    num_requests = request.session.get('num_requests', 0) + 1
+    
+    # save changes to the variable
+    request.session['num_requests'] = num_requests
+    
+    # if num requests > 4: delete cookie
+    if num_requests >=4: del(request.session['num_requests'])
+    
+    # generate response
+    response  = HttpResponse(f"num requests: {num_requests}")
+    
+    # set arbitraty cookie
+    response.set_cookie("my_cookie", "1337", max_age=1000)
+    
+    return response
